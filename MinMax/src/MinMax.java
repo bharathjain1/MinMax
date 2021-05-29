2c3f18e9ea91c7bea64417681fd3be73a78fe5f0
bharathjain1 <39914910+bharathjain1@users.noreply.github.com>
2021-05-29 17:04:30 +0530

    Update MinMax.java

--- MinMax/src/MinMax.java
+++ MinMax/src/MinMax.java
@@ -3,7 +3,7 @@ public class MinMax {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the test cases");
+		System.out.println("enter the test cases");// Enter the number of test cases
 		int t=sc.nextInt();
 		System.out.println("enter the number of elements");
 		for(int a=0;a<t;a++)
@@ -12,11 +12,11 @@ public class MinMax {
 			int ar[]=new int[n];
 			for(int i=0;i<n;i++)
 			{
-				ar[i]=sc.nextInt();
+				ar[i]=sc.nextInt();//Enter the number of elements
 			}
 			
-			Arrays.sort(ar);
-			System.out.println(ar[0]*(n-1));
+			Arrays.sort(ar);                //sorting array
+			System.out.println(ar[0]*(n-1));//Basic logic is multiplying first element and last
 			
 		}
 	}
